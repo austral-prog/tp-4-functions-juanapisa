92529f0087b0687e3182975f40d58c3780ae5d9f
juanapisa <jipisa@mail.austral.edu.ar>
2026-04-06 12:19:55 -0300

    Update number_to_month.py

--- number_to_month.py
+++ number_to_month.py
@@ -1,4 +1,30 @@
 # Replace the "ANSWER HERE" for your answer
 
 def number_to_month(month):
-    return "ANSWER HERE" # Remove this line and implement
+    if month == 1:
+        mes = "enero"
+    elif month == 2:
+        mes = "febrero"
+    elif month == 3:
+        mes = "marzo"
+    elif month == 4:
+        mes = "abril"
+    elif month == 5:
+        mes = "mayo"
+    elif month == 6:
+        mes = "junio"
+    elif month == 7:
+        mes = "julio"
+    elif month == 8:
+        mes = "agosto"
+    elif month == 9:
+        mes = "septiembre"
+    elif month == 10:
+        mes = "octubre"
+    elif month == 11:
+        mes = "noviembre"
+    elif month == 12:
+        mes = "diciembre"
+    else:
+        mes = "error"
+    return mes
